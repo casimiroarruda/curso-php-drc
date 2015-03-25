7670c244aaeb607a13a17b89caae885de68adf36
Anderson Casimiro <duodraco@gmail.com>
2015-03-25 20:31:40 -0300

    db

---
+++ aulas/07-bancos de dados/old.php
@@ -0,0 +1,22 @@
+<?php
+$conn = mysql_connect('localhost','web_user','xurumelas',true);
+if(!$conn){
+    die("Algo deu muito errado: ".mysql_error());
+}
+mysql_select_db('report', $conn);
+$data = array(
+    "Mastercard" => rand(1,1000000000)/100,
+    "Visa" => rand(1,1000000000)/100,
+    "Diners" => rand(1,1000000000)/100,
+    "Amex" => rand(1,1000000000)/100
+);
+/*foreach($data as $paymentType => $value){
+    $sql = "INSERT INTO line (payment_type,amount) VALUES ('$paymentType',$value)";
+    $x = mysql_query($sql,$conn);
+}*/
+
+$qry = mysql_query("SELECT * FROM line");
+while($obj = mysql_fetch_object($qry)){
+    echo "{$obj->id}, {$obj->payment_type}: USD {$obj->amount}",PHP_EOL;
+}
+mysql_close($conn);
