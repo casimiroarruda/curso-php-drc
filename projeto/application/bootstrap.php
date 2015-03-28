86dd460dd374b3a00c75462ac9b16fac9781de8f
Anderson Casimiro <duodraco@gmail.com>
2015-03-27 21:44:16 -0300

    projeto final - pt2

--- projeto/application/bootstrap.php
+++ projeto/application/bootstrap.php
@@ -1,15 +1,15 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
-
+date_default_timezone_set('America/Sao_Paulo');
+use Symfony\Component\HttpFoundation\Request;
 $settings = parse_ini_file(__DIR__ . "/configuration/application.ini", true);
-
 $container = new Pimple;
 $container['settings'] = $settings;
 $container['pdo'] = function($c){
     return new PDO(
-        $c['settings']['dsn'],
-        $c['settings']['username'],
-        $c['settings']['password']
+        $c['settings']['db']['dsn'],
+        $c['settings']['db']['username'],
+        $c['settings']['db']['password']
     );
 };
 $app = new Silex\Application;
@@ -20,7 +20,9 @@ $app->register(new Silex\Provider\TwigServiceProvider(), [
 $app['debug']=true;
 //ROTAS
 $app->get('/','DRC\ToDo\Controller\Task::listAll');
+$app->get('/new','DRC\ToDo\Controller\Task::newTask');
+$app->put('/api/task','DRC\ToDo\Controller\Task::insert');
 
 
-
+Request::enableHttpMethodParameterOverride();
 $app->run();
--- projeto/application/library/DRC/ToDo/Controller/Task.php
+++ projeto/application/library/DRC/ToDo/Controller/Task.php
@@ -8,6 +8,50 @@ class Task
 {
     public function listAll(Request $request, Application $app)
     {
-        return $app['twig']->render('index.twig', []);
+        /**
+         * @var \PDO $pdo
+         */
+        $pdo = $app['container']['pdo'];
+        $list = $pdo->prepare(
+            'SELECT id,title,description,status,creation_date as creationDate,last_modified as lastModified
+               FROM task
+              ORDER BY status DESC,creation_date ASC');
+        $list->setFetchMode(\PDO::FETCH_CLASS, 'DRC\ToDo\Data\Task');
+        $list->execute();
+        $data = [
+            'title' => 'DRC\ToDo :: listagem',
+            'tasks' => $list->fetchAll()
+        ];
+        return $app['twig']->render('index.twig', $data);
+    }
+
+    public function newTask(Request $request, Application $app)
+    {
+        return $app['twig']->render('new.twig', ['title' => 'DRC\ToDo :: Nova Tarefa']);
+    }
+
+    public function insert(Request $request, Application $app)
+    {
+        /**
+         * @var \PDO $pdo
+         */
+        $pdo = $app['container']['pdo'];
+        $insert = $pdo->prepare(
+            "INSERT INTO task
+            (title,description,status,creation_date,last_modified) VALUES
+            (:title,:description,:status,:creation_date,:last_modified)"
+        );
+        $now = date('Y-m-d H:i:s');
+        $success = $insert->execute([
+            'title' => $request->get('title'),
+            'description' => $request->get('description'),
+            'status' => \DRC\ToDo\Data\Task::STATUS_ONGOING,
+            'creation_date' => $now,
+            'last_modified' => $now
+        ]);
+        if($success){
+            return $app->redirect('/');
+        }
+        return '<a href="javascript:history.back()">Erro, tentar novamente</a>';
     }
 }
